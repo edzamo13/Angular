ac9f7d4b5b377d0666e897b5bbb818b65d3924ae
edzamo13@gmail.com <ezamora@mag.gob.ec>
2023-02-06 17:43:57 -0500

    Update of folfer

--- nodeJs/nodeJSExample/ejemplo1/src/client.js
+++
@@ -1,8 +0,0 @@
-import net from 'net';
-
-const socket = net.Socket();
-socket.connect(8000, 'localhost');
-
-socket.write('hola?');
-
-socket.on('data', data => console.log(data.toString()));
--- nodeJs/nodeJSExample/ejemplo1/src/ejemplo1.js
+++
@@ -1,5 +0,0 @@
-console.log('hola mundo');
-const value = 24;
-
-const miFn = num => num * 10;
-console.log(miFn(value));
