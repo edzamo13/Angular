76f9840f44122c3214d6cb936fb244c4a6539bef
EDWIN ZAMORA <ezamoram@pichincha.com>
2024-06-22 14:25:57 -0500

    add new feature with angular 15

--- pluralsight/app-pluralsight/src/app/products/product-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'pm-product',
-  templateUrl: './product-list.component.html',
-})
-export class ProductListComponent {
-  pageTitle: string = 'Product List';
-}
